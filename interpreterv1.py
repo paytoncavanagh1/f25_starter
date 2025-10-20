9c71dbb36ec8143c74ca7fd144ef0b9ee4efba85
Payton Cavanagh <paytoncavanagh34@gmail.com>
2025-10-19 20:29:32 -0700

    finished all test cases

--- interpreterv1.py
+++ interpreterv1.py
@@ -72,7 +72,7 @@ class Interpreter(InterpreterBase):
             )
         
         expression = statement_node.get("expression")
-        val = str(self.expression_evaluater(expression))
+        val = self.expression_evaluater(expression)
         self.variable_name_to_value[expression_var] = val
 
     def expression_evaluater(self, expression):
@@ -113,41 +113,33 @@ class Interpreter(InterpreterBase):
     def evaluate_binary_operator(self, expression):
         if expression.elem_type == "+":
             op1 = expression.get("op1")
-            if op1.elem_type != InterpreterBase.QUALIFIED_NAME_NODE:
-                op1_val = int(op1.get("val"))
-            else:
-                op1_var = op1.get("name")
-                op1_val = int(self.variable_name_to_value[op1_var])
-
             op2 = expression.get("op2")
-            if op2.elem_type != InterpreterBase.QUALIFIED_NAME_NODE:
-                op2_val = int(op2.get("val"))
-            else:
-                op2_var = op2.get("name")
-                op2_val = int(self.variable_name_to_value[op2_var])
-            
-            if op1.elem_type == InterpreterBase.STRING_NODE and op2.elem_type == InterpreterBase.INT_NODE or op1.elem_type == InterpreterBase.INT_NODE and op2.elem_type == InterpreterBase.STRING_NODE:
+
+            first_op = self.expression_evaluater(op1)
+            second_op = self.expression_evaluater(op2)
+
+            if not (isinstance(first_op, int) and isinstance(second_op, int)):
                 super().error(
                     ErrorType.TYPE_ERROR,
                     "Incompatible types for arithmetic operation",
                 )
             else:
-                return op1_val + op2_val
+                return first_op + second_op
 
         elif expression.elem_type == "-":
             op1 = expression.get("op1")
-            op1_val = op1.get("val")
-
             op2 = expression.get("op2")
-            op2_val = op2.get("val")
 
-            if op1.elem_type == InterpreterBase.STRING_NODE and op2.elem_type == InterpreterBase.INT_NODE or op1.elem_type == InterpreterBase.INT_NODE and op2.elem_type == InterpreterBase.STRING_NODE:
+            first_op = self.expression_evaluater(op1)
+            second_op = self.expression_evaluater(op2)
+
+            if not (isinstance(first_op, int) and isinstance(second_op, int)):
                 super().error(
                     ErrorType.TYPE_ERROR,
                     "Incompatible types for arithmetic operation",
                 )
             else:
-                return op1_val - op2_val
+                return first_op - second_op
 
     def is_func_call(self, statement_node):
         if statement_node.elem_type == InterpreterBase.FCALL_NODE:
@@ -167,7 +159,7 @@ class Interpreter(InterpreterBase):
                 if arg.elem_type == InterpreterBase.QUALIFIED_NAME_NODE:
                     var_name = arg.get("name")
                     if var_name in self.variable_name_to_value:
-                        final_str += self.variable_name_to_value[var_name]
+                        final_str += str(self.variable_name_to_value[var_name])
                     else:
                         super().error(
                             ErrorType.NAME_ERROR,
@@ -184,25 +176,9 @@ class Interpreter(InterpreterBase):
             for arg in arg_list:
                 final_str += str((arg.get("val")))
                 self.output(final_str)
-                return str(self.get_input())
+                return self.get_input()
         else:
             super().error(
                 ErrorType.NAME_ERROR,
                 f"Function {statement_node.get("name")} has not been defined",
             )
-
-	# def do_assignment(self, statement_node):
-	# 	target_var_name = get_target_variable_name(statement_node)
-    #     if !var_name_exists(target_var_name):
-    #         generate_error("Undefined variable " + target_var_name)
-	# 	source_node = get_expression_node(statement_node)
-	# 	resulting_value = evaluate_expression(source_node)
-	# 	this.variable_name_to_value[target_var_name] = resulting_value
-     
-    # def evaluate_expression(self, expression_node):
-    #     if is_value_node(expression_node):
-    #         return get_value(expression_node)
-    #     else if is_variable_node(expression_node):
-    #         return get_value_of_variable(expression_node)
-    #     else if is_binary_operator(expression_node):
-    #         return evaluate_binary_operator(expression_node)
